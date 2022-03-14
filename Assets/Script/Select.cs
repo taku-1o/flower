5d9d3041afe1fda497e2dbd11480114ccbdfc678
木下 <C0120@oic.jp>
2022-03-14 18:43:48 +0900

    Revert "ハチ調整途中"
    This reverts commit a7d2528575e5f7fa5f973955c4aa04e2efccbf81.

--- Assets/Script/Select.cs
+++ Assets/Script/Select.cs
@@ -8,9 +8,6 @@ public class Select : MonoBehaviour
 {
     public Button FirstSelectButton;
 
-    public BoxCollider2D col;
-    public BoxCollider2D col2;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -41,8 +38,6 @@ public class Select : MonoBehaviour
       
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            col.enabled = false;
-            col2.enabled = false;
             MyFadeManager.Instance.LoadScene("Game", 1.0f, true);
 
         }
@@ -50,8 +45,6 @@ public class Select : MonoBehaviour
     }
     public void ClickRetry()
     {
-        col.enabled = false;
-        col2.enabled = false;
         MyFadeManager.Instance.LoadScene("Game", 1.0f, true);
     }
 
@@ -60,18 +53,17 @@ public class Select : MonoBehaviour
        
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            col.enabled = false;
-            col2.enabled = false;
             MyFadeManager.Instance.LoadScene("SampleScene", 1.0f,true);
 
         }
-        
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    MyFadeManager.Instance.LoadScene("SampleScene", 1.0f, true);
+        //}
     }
 
     public void Clickta()
     {
-        col.enabled = false;
-        col2.enabled = false;
         MyFadeManager.Instance.LoadScene("SampleScene", 1.0f, true);
     }
 }
